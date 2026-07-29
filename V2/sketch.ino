543c2b1d5cd43c4732d67ec19689b63cde62229c
Reigh Vhen Dimailig <reighvhend@gmail.com>
2026-07-29 00:06:09 -0600

    Implement button to toggle mute on RDA5807
    Added functionality for a button to toggle mute state.

--- V2/sketch.ino
+++ V2/sketch.ino
@@ -1,1 +1,33 @@
+//adding a on and off button
+#include <RDA5807.h>
+RDA5807 rx;
+#define BUTTON_PIN 18
 
+int lastButtonState;
+
+void setup(){
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  lastButtonState = digitalRead(BUTTON_PIN);
+  
+  rx.setup();
+  rx.setFrequency(9170);
+  rx.setVolume(1);
+  rx.setMute(true);
+  
+}
+
+void loop(){
+  // read the value of the button
+  int buttonState = digitalRead(BUTTON_PIN);
+  
+  bool currentState = rx.getMute();
+
+  if (lastButtonState==HIGH && buttonState==LOW) {//state change
+
+    rx.setMute(!currentState);
+
+    delay(50)
+    
+  }
+  lastButtonState = buttonState;
+}
