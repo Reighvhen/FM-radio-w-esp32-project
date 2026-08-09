b75cb013a2d16ea09463bea072c90d201b5c68da
willwork4bread <217871890+Reighvhen@users.noreply.github.com>
2026-08-08 23:43:16 -0600

    Refactor sketch.ino by removing unused buttons
    Removed unused button definitions and cleaned up comments for clarity.

--- V3/V3.2/sketch.ino
+++ V3/V3.2/sketch.ino
@@ -1,4 +1,3 @@
-// UNEDITED -- but it works (need to be cleaned up)
 // this has 1 led and a potentiometer for volume control (limit up to volume 4)
 //The execution order is:
                           // Global variable declarations/initializations — once, at the very start
@@ -9,15 +8,11 @@
 #include <RDA5807.h>
 RDA5807 rx;
 #define Bmute 18
-#define Bup 4
-#define Bdown 5
 #define red 16
 #define green 17
 #define blue 19
 
 byte lastMuteState;
-byte lastUp;
-byte lastDown;
 
 bool isOn = false; //
 
@@ -30,11 +25,6 @@ void setup()
                                         //— before any button has been pressed. 
                                         // HIGH = not pressed, LOW = pressed cause its going to connect to GND thus giving us 0V
 
-  pinMode(Bup, INPUT_PULLUP);
-  lastUp = digitalRead(Bup);
-
-  pinMode(Bdown, INPUT_PULLUP);
-  lastDown = digitalRead(Bdown);
 
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
@@ -44,10 +34,10 @@ void setup()
   digitalWrite(blue, LOW);
   
   //-----------------------------
-  Serial.begin(115200);
+  Serial.begin(115200); // for debugging 
   rx.setup(); //call rx.powerUp: initialize I2C communication & (DMUTE, DHIZ, ENABBLE)=1, & VOLUME=0
   rx.setFrequency(9170); //Freq of the radio we want to tune in
-  rx.setMute(true); //true=muted, since the rx.setup unmutes it, we want to unmute it again so that it would be quiet when just plug in to power supply
+  rx.setMute(true); //true=muted; since the rx.setup unmutes it, we want to unmute it again so that it would be quiet when just plug in to power supply
 
 }
 
@@ -62,16 +52,16 @@ void loop()
       // 2. LOW -> HIGH.
       // 3. HIGH -> LOW.
       // 4. HIGH -> HIGH.
-        // if last =! current state, it means that button has been pressed, 
-        // if it was equal it means that button was not pressed cause the state stayed the same
+        // if last =! current state, it means that the button has been pressed, 
+        // if it was equal, it means that the button was not pressed cause the state stayed the same
   // we only enter the next block of code if the current and last state are different
   if (lastMuteState != nowMuteState) 
   { 
     
     //At this point we have only 2 possibilities left:
-      // 1. Either the previous state was LOW and the current state is HIGH (pressed to not pressed). --> release
-      // 2. Or the previous state was HIGH and the current state is LOW (not pressed to pressed). --> push
-    // We interested in the moment the button goes down (pressed), not when it comes back up. 
+      // 1. Either the previous state was LOW, and the current state is HIGH (pressed to not pressed). --> release
+      // 2. Or the previous state was HIGH, and the current state is LOW (not pressed to pressed). --> push
+    // We are interested in the moment the button goes down (pressed), not when it comes back up. 
       // Because ppl expect things to happen when button is pushed and not when they release.
     if (isOn == false && lastMuteState == HIGH && nowMuteState == LOW ) // if are off (currently) and press the button == turning ON
     {
@@ -87,7 +77,7 @@ void loop()
 
     else if (isOn == true && lastMuteState == HIGH && nowMuteState == LOW ) // if are on and press the button == turning OFF
     {
-      //rx.powerDown(); // Power the receiver off: doesnt touch the frequency, thus theres no need to initialize the freq again
+      //rx.powerDown(); // Power the receiver off: doesn't touch the frequency, thus theres no need to initialize the freq again
       rx.setMute(true);
             
       //turning on the red led and off the green
@@ -103,10 +93,6 @@ void loop()
     //toggle the isOn state to get the current state of the isOn; prev, we are using its prev state to check the next step
    // isOn = !isOn;
   }
-
-
-  byte nowUp = digitalRead(Bup); // read the current state of the volume up button
-  byte nowDown = digitalRead(Bdown); // read the current state of the volume down button
   
   if (isOn == true) // only if we are on can we use the volume potentiometer
   {
